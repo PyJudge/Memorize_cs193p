b99652b1cdde89ad1384e198caba00cdcb3497a1
Won Myeong, Kwon <wonmyeong.kwon@gmail.com>
2020-05-24 21:07:35 +0900

    Lec1, 2 Original Src

---
+++ Memorize_cs193p/EmojiMemoryGame.swift
@@ -0,0 +1,35 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize_cs193p
+//
+//  Created by LatteFairy on 2020/05/24.
+//  Copyright © 2020 Seoul High Court. All rights reserved.
+//
+// Making a VM df
+
+import Foundation
+
+class EmojiMemoryGame{
+    private var model : MemoryGame<String> = createMemoryGame()
+    
+    static func createMemoryGame() -> MemoryGame<String>{
+        let emojis   = ["👻", "🎃", "🥰"]
+
+        return MemoryGame<String>(numOfPairsOfCards: emojis.count){
+            pairIndex in emojis[pairIndex]
+        }
+    }
+    
+    // MARK: -- Access to the Model
+    var cards : Array<MemoryGame<String>.Card> {
+        model.cards
+    }
+    // MARK: -- Intent(s)
+    
+    func choose(card:MemoryGame<String>.Card){
+        model.choose(card: card)
+    }
+    
+    
+    
+}
---
+++ Memorize_cs193p/MemoryGame.swift
@@ -0,0 +1,40 @@
+//
+//  MemoryGame.swift
+//  Memorize_cs193p
+//
+//  Created by LatteFairy on 2020/05/24.
+//  Copyright © 2020 Seoul High Court. All rights reserved.
+//
+
+// Building a MODEL
+
+import Foundation
+
+struct MemoryGame<CardContent> {
+    
+    var cards : Array <Card>
+
+    func choose(card : Card){
+        print(" chosen card \(card)")
+                
+//        card.isFaceUp = !card.isFaceUp
+    }
+    
+    init(numOfPairsOfCards:Int , cardContentFactory : (Int) -> CardContent){
+        cards = Array<Card>()
+        for pairIndex in 0..<numOfPairsOfCards{
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content, id : 2*pairIndex))
+            cards.append(Card(content: content, id : 2*pairIndex+1))
+
+        }
+    }
+
+    struct Card: Identifiable{
+        var isFaceUp : Bool = true
+        var content : CardContent
+        var isMatched : Bool = false
+        var id : Int
+    }
+
+}
