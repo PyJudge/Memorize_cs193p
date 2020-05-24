74ec1c607c2308ab8c257cb144c0a97764a0778e
Won Myeong, Kwon <wonmyeong.kwon@gmail.com>
2020-05-24 20:59:20 +0900

    Initial Commit

---
+++ Memorize_cs193p/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize_cs193p
+//
+//  Created by LatteFairy on 2020/05/24.
+//  Copyright © 2020 Seoul High Court. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
