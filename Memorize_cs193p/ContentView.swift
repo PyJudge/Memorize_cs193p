88c549c813f2a59aeb763c3a399cf39543fbafc3
PyJudge <wonmyeong.kwon@gmail.com>
2020-07-10 00:56:46 +0900

    LEC 3 original src
    done for lec 3,

--- Memorize_cs193p/ContentView.swift
+++ Memorize_cs193p/ContentView.swift
@@ -17,11 +17,11 @@ struct ContentView: View {
                 CardView(card: card).onTapGesture {
                     self.viewModel.choose(card: card)
                 }
+            Divider()
             }
         }
             .padding()
             .foregroundColor(Color.orange)
-            .font(Font.largeTitle) //Original Code
 
     }
 }
@@ -30,15 +30,31 @@ struct CardView: View{
     var card : MemoryGame<String>.Card
     
     var body : some View{
+        GeometryReader { geometry in
+            self.body(for: geometry.size) //geometry.size ~ CGSize (which has width and height in it)
+        }
+    }
+    
+    func body(for size: CGSize) -> some View  {
         ZStack{
-            if card.isFaceUp{
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
-                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
-                Text(card.content)
+            if self.card.isFaceUp{
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                Text(self.card.content)
             }else{
-                RoundedRectangle(cornerRadius: 10.0).fill(Color.orange)
+                RoundedRectangle(cornerRadius: cornerRadius).fill(Color.orange)
             }
         }
+            .font(Font.system(size: fontSize(for: size))) //Original Code
+
+    }
+    
+    // MARK: -- Drawing Constants
+    
+    let cornerRadius : CGFloat = 10.0 // Should be CGFloat, not int or double, to use inside Rounded Rectangle
+    let edgeLineWidth : CGFloat = 3
+    func fontSize(for size : CGSize) -> CGFloat{
+        min(size.width, size.height) * 0.75
     }
 }
 
